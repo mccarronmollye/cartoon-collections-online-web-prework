03bdbfabdc680cc62e3bb1d7c8a30f088890cec1
Molly McCarron <mccarronmollye@gmail.com>
2018-11-07 14:30:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,7 @@ end
 end
 
 def find_the_cheese(array)
-cheese_types = ["cheddar", "gouda", "camembert"]
 array.find do |array|
-  array.include?(cheese_types)
+  array.include?("cheddar", "gouda", "camembert")
 end
 end
