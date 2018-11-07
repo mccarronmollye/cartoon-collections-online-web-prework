175fe0745f030dfd821f0a31ed8f9a4ebb1871ae
Molly McCarron <mccarronmollye@gmail.com>
2018-11-07 13:59:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,11 @@ def roll_call_dwarves(array)
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+  list.collect do |strength|
+    strength.capitalize
+    strength + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
